5aa4d4aee543117d303af6d02f599cc1763b1200
haru <harucq8@gmail.com>
2025-08-09 18:58:22 -0500

    correcciones

--- scenes/second_scene.gd
+++ scenes/second_scene.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-@onready var cam = $Camera2D
-var move_amount := 200 # píxeles por click
+@export var fondo: TextureRect
+var move_amount := 30 # píxeles por click
 var left_limit := 0
-var right_limit := 3840 - 1080  # ancho fondo - ancho pantalla
+var right_limit := 3840 - 1080  
 
 func _on_flecha_izquierda_pressed():
-	cam.position.x = max(cam.position.x - move_amount, left_limit)
-
+	fondo.global_position.x = max(fondo.global_position.x + move_amount, left_limit)
+	
 func _on_flecha_derecha_pressed():
-	cam.position.x = min(cam.position.x + move_amount, right_limit)
+	fondo.global_position.x = min(fondo.global_position.x - move_amount, left_limit)
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,9 +1,5 @@
 extends Control
 
-func _ready():
-	# Connect the TextureButton's pressed signal
-	$MarginContainer/HBoxContainer/Button.pressed.connect(_on_button_pressed)
-
 func _on_button_pressed():
 	# Change scene to second_scene.tscn
 	get_tree().change_scene_to_file("res://scenes/second_scene.tscn")
