ab29d353b00361f9a1ef0d2713310b34d56be557
angel4uu <angela.jara@unmsm.edu.pe>
2025-08-10 13:08:35 -0500

    add game scenes

--- scripts/second_scene.gd
+++ scripts/second_scene.gd
@@ -2,11 +2,22 @@ extends Node2D
 
 @export var fondo: TextureRect
 @export var Paleta: TextureRect
+@export var paleta_dedo: TextureRect
+
 @export var pastel_dedo: TextureRect
 @export var pastel_dedo_centro: TextureRect
-@export var vidas: TextureRect
+@export var dedo:TextureRect
+
 @export var flecha_derecha: TextureButton
 @export var flecha_izquierda: TextureButton
+
+@export var vidas: TextureRect
+@export var tres_vidas : TextureRect
+@export var dos_vidas : TextureRect
+@export var una_vidas : TextureRect
+
+@export var blur_background:TextureRect
+
 
 var move_amount := 30 # píxeles por click
 var left_limit := 0
@@ -53,10 +64,56 @@ func _on_paleta_gui_input(event: InputEvent) -> void:
 func _on_control_gui_input(event: InputEvent) -> void:
 	pass # Replace with function body.
 
-func _on_pastel_dedo(event:InputEvent):
+var clicked := false
+func _on_pastel_dedo(event: InputEvent):
 	if event is InputEventMouseButton and event.pressed:
-		pastel_dedo_centro.visible=true
-		vidas.visible=true
-		flecha_derecha.visible=false
-		flecha_izquierda.visible=false
+		pastel_dedo_centro.visible = true
+		vidas.visible = true
+		flecha_derecha.visible = false
+		flecha_izquierda.visible = false
+		blur_background.visible=true
+		var miss_count = 0
+
+		await get_tree().create_timer(2.0).timeout
+		
+		#juego
+		for i in range(1, 8):
+			var circulo = get_node("UI/Control/pastel_dedo_centro/circulos%d" % i)
+			var anim_player = circulo.get_node("AnimationPlayer")
+
+			clicked = false
+			circulo.visible = true
+			anim_player.play("hacer_pequeño")
+
+			await get_tree().create_timer(4).timeout
+
+			if not clicked:
+				miss_count += 1
+
+				if miss_count==1:
+					tres_vidas.visible=false
+				if miss_count==2:
+					dos_vidas.visible=false
+				if miss_count==3:
+					una_vidas.visible=false
+				print("Missed:", i)
+
+			circulo.visible = false
+
+			if miss_count >= 3:
+				print("Stopped early: 3 misses")
+				#game over
+				break
+				
+		miss_count=0
+		
+		
+		#victoria: objeto obtenido
+		pastel_dedo_centro.visible=false
+		paleta_dedo.visible=true
+		dedo.visible=true
+		
+
+func set_clicked():
+	clicked = true
 		
