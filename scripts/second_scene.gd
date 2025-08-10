610b7b0f3a07b3318c01066f0e0f814432f1921b
angel4uu <angela.jara@unmsm.edu.pe>
2025-08-10 15:45:07 -0500

    add 3 games

--- scripts/second_scene.gd
+++ scripts/second_scene.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
 @export var fondo: TextureRect
+
 @export var Paleta: TextureRect
 @export var paleta_dedo: TextureRect
+@export var paleta_dedo_sangre: TextureRect
+@export var paleta_dedo_sangre_ojo: TextureRect
 
 @export var pastel_dedo: TextureRect
 @export var pastel_dedo_centro: TextureRect
 @export var dedo:TextureRect
+
+@export var leche_sangre_centro: TextureRect
+@export var sangre:TextureRect
+
+@export var cupcake_ojo_centro: TextureRect
+@export var ojo:TextureRect
 
 @export var flecha_derecha: TextureButton
 @export var flecha_izquierda: TextureButton
@@ -61,9 +70,6 @@ func _on_paleta_gui_input(event: InputEvent) -> void:
 		# $UI/Control/Paleta.size *= Vector2(1.2, 1.2) 
 
 
-func _on_control_gui_input(event: InputEvent) -> void:
-	pass # Replace with function body.
-
 var clicked := false
 func _on_pastel_dedo(event: InputEvent):
 	if event is InputEventMouseButton and event.pressed:
@@ -99,10 +105,12 @@ func _on_pastel_dedo(event: InputEvent):
 				print("Missed:", i)
 
 			circulo.visible = false
-
+			
+			#game over
 			if miss_count >= 3:
 				print("Stopped early: 3 misses")
-				#game over
+				await get_tree().create_timer(4).timeout
+				get_tree().change_scene_to_file("res://scenes/GameOver.tscn")
 				break
 				
 		miss_count=0
@@ -112,6 +120,114 @@ func _on_pastel_dedo(event: InputEvent):
 		pastel_dedo_centro.visible=false
 		paleta_dedo.visible=true
 		dedo.visible=true
+		await get_tree().create_timer(4).timeout
+		get_tree().change_scene_to_file("res://scenes/intro_2.tscn")
+		
+func _on_leche_sangre(event: InputEvent):
+	if event is InputEventMouseButton and event.pressed:
+		leche_sangre_centro.visible = true
+		vidas.visible = true
+		flecha_derecha.visible = false
+		flecha_izquierda.visible = false
+		blur_background.visible=true
+		var miss_count = 0
+
+		await get_tree().create_timer(2.0).timeout
+		
+		#juego
+		for i in range(1, 8):
+			var circulo = get_node("UI/Control/leche_sangre_centro/circulos%d" % i)
+			var anim_player = circulo.get_node("AnimationPlayer")
+
+			clicked = false
+			circulo.visible = true
+			anim_player.play("hacer_pequeño")
+
+			await get_tree().create_timer(4).timeout
+
+			if not clicked:
+				miss_count += 1
+
+				if miss_count==1:
+					tres_vidas.visible=false
+				if miss_count==2:
+					dos_vidas.visible=false
+				if miss_count==3:
+					una_vidas.visible=false
+				print("Missed:", i)
+
+			circulo.visible = false
+			
+			#game over
+			if miss_count >= 3:
+				print("Stopped early: 3 misses")
+				await get_tree().create_timer(4).timeout
+				get_tree().change_scene_to_file("res://scenes/GameOver.tscn")
+				break
+				
+		miss_count=0
+		
+		
+		#victoria: objeto obtenido
+		leche_sangre_centro.visible=false
+		paleta_dedo_sangre.visible=true
+		sangre.visible=true
+		await get_tree().create_timer(4).timeout
+		get_tree().change_scene_to_file("res://scenes/intro_3.tscn")
+		
+		
+func _on_cupcake_ojo(event: InputEvent):
+	if event is InputEventMouseButton and event.pressed:
+		cupcake_ojo_centro.visible = true
+		vidas.visible = true
+		flecha_derecha.visible = false
+		flecha_izquierda.visible = false
+		blur_background.visible=true
+		var miss_count = 0
+
+		await get_tree().create_timer(2.0).timeout
+		
+		#juego
+		for i in range(1, 8):
+			var circulo = get_node("UI/Control/cupcake_ojo_centro/circulos%d" % i)
+			var anim_player = circulo.get_node("AnimationPlayer")
+
+			clicked = false
+			circulo.visible = true
+			anim_player.play("hacer_pequeño")
+
+			await get_tree().create_timer(4).timeout
+
+			if not clicked:
+				miss_count += 1
+
+				if miss_count==1:
+					tres_vidas.visible=false
+				if miss_count==2:
+					dos_vidas.visible=false
+				if miss_count==3:
+					una_vidas.visible=false
+				print("Missed:", i)
+
+			circulo.visible = false
+			
+			#game over
+			if miss_count >= 3:
+				print("Stopped early: 3 misses")
+				await get_tree().create_timer(4).timeout
+				get_tree().change_scene_to_file("res://scenes/GameOver.tscn")
+				break
+				
+		miss_count=0
+		
+		
+		#victoria: objeto obtenido
+		cupcake_ojo_centro.visible=false
+		paleta_dedo_sangre_ojo.visible=true
+		ojo.visible=true
+		await get_tree().create_timer(4).timeout
+		get_tree().change_scene_to_file("res://scenes/final.tscn")
+
 		
 
 func set_clicked():
