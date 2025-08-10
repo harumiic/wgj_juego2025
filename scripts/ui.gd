aec436e2226e833dfd47107fdb324f1e7ef226cb
haru <harucq8@gmail.com>
2025-08-10 13:03:59 -0500

    Redireccionamiento de frames

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -9,3 +9,17 @@ func _on_play_pressed():
 
 func _on_button_game():
 	get_tree().change_scene_to_file("res://scenes/second_scene.tscn")
+
+func _on_quit_pressed():
+	get_tree().change_scene_to_file("res://scenes/Quit.tscn")
+
+func _on_button_quit_game():
+	get_tree().quit()
+		
+#func _on_credits_pressed():
+	#print('hola')
+	#get_tree().change_scene_to_file("res://scenes/Creditos.tscn")
+
+
+func _on_credits_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/Creditos.tscn")
